ef05b42879cdd4606aa195fb9d2f36d7eeeac4a0
PalashGharde <39770100+PalashGharde@users.noreply.github.com>
2025-07-11 17:11:57 -0700

    Add State architecture

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,36 +1,59 @@
 import "./App.css";
+import { useEffect, useReducer } from "react";
 import palash from "./assets/face imgs.jpg";
 
-function NewComponent({ name, numgames }) {
+function NewComponent({ numprojects, currentRole }) {
   // console.log(props);
   return (
     <header>
       <h1>
-        {name} made {numgames} games
+        I made {numprojects} {currentRole ? "games" : "projects"}
       </h1>
     </header>
   );
 }
 
 let games = ["Kidnapped", "Wrongway", "Jetman Adventures", "Hunting simulator"];
+let projects = [
+  "ASL detection",
+  "Web applcation",
+  "Lung disease classification",
+  "Face Recognition with AWS",
+  "E commerce Website",
+];
 
 const gameObjects = games.map((game, i) => ({
   id: i,
   title: game,
 }));
 
-function Main({ gamelist }) {
+const projectObjects = projects.map((project, i) => ({
+  id: i,
+  title: project,
+}));
+
+function Main() {
   return (
     <>
       <div>
-        <h2>Welcome to my Portfolio</h2>
+        <h1>Welcome to my Portfolio</h1>
       </div>
       <main>
         <img src={palash} height={300} alt="Photo of Palash (me)" />
+      </main>
+    </>
+  );
+}
+
+function ProjectList({ currentRole }) {
+  let currList = currentRole ? gameObjects : projectObjects;
+  return (
+    <>
+      <main>
         <ul>
-          {gamelist.map((game) => (
-            <li key={game.id} style={{ listStyleType: "none" }}>
-              {game.title}
+          {currList.map((project) => (
+            <li key={project.id} style={{ listStyleType: "none" }}>
+              {project.title}
             </li>
           ))}
         </ul>
@@ -40,10 +63,22 @@ function Main({ gamelist }) {
 }
 
 function App() {
+  const [Role, toggle] = useReducer((Role) => !Role, true);
+
   return (
     <div>
-      <NewComponent name="Palash" numgames={10} />
-      <Main gamelist={gameObjects} />
+      <Main />
+      <h2>
+        You are looking for {Role ? "Game Developer" : "Software Developer"}.
+      </h2>
+      <button onClick={toggle}>
+        Do you need {!Role ? "Game Developer" : "Software Developer"} ?
+      </button>
+      <NewComponent
+        numprojects={Role ? games.length : projects.length}
+        currentRole={Role}
+      />
+      <ProjectList currentRole={Role} />
     </div>
   );
 }
